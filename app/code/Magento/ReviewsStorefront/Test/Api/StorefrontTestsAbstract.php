49989413df510214e9964b32b733f93caec9368a
Aleksejs Baranovs <alexbar199516@gmail.com>
2020-11-10 17:35:45 +0200

    Support for ratings & reviews in Storefront App - Tests

--- app/code/Magento/ReviewsStorefront/Test/Api/StorefrontTestsAbstract.php
+++ app/code/Magento/ReviewsStorefront/Test/Api/StorefrontTestsAbstract.php
@@ -15,7 +15,7 @@ use Magento\ReviewsStorefront\Model\Storage\State;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Helper\CompareArraysRecursively;
-use Magento\TestFramework\Workaround\ReviewsConsumerInvoker;
+use Magento\TestFramework\Workaround\ConsumerInvoker;
 use PhpAmqpLib\Exception\AMQPTimeoutException;
 use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\TestCase;
@@ -179,7 +179,7 @@ abstract class StorefrontTestsAbstract extends TestCase
     protected function runTest()
     {
         if (!$this->isSoap()) {
-            Bootstrap::getObjectManager()->create(ReviewsConsumerInvoker::class)->invoke();
+            Bootstrap::getObjectManager()->create(ConsumerInvoker::class)->invoke();
             $this->dataDefinition->refreshDataSource($this->storageState->getCurrentDataSourceName(['review']));
 
             parent::runTest();
